a9315444518c14360654959e402d9f8e9195e2a1
educunhamxk <92655692+educunhamxk@users.noreply.github.com>
2023-06-08 17:38:30 -0300

    Add files via upload

--- app.py
+++ app.py
@@ -1,10 +1,4 @@
 import streamlit as st
-import pandas as pd
-import matplotlib.pyplot as plt
-import seaborn as sns
-import pycaret
-from pycaret.regression import *
-from sklearn.pipeline import Pipeline
 
 
 #configuração da página
